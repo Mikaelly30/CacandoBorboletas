311c63ad6b2727985d226d3f1b3842c16cfcb372
Mikaelly30 <90416850+Mikaelly30@users.noreply.github.com>
2022-01-14 14:14:19 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,188 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var jogador;
+var grupoBorboletas;
+var borboleta1Img;
+var borboleta2Img;
+var borboleta3Img;
+var borboleta4Img;
+var backgroundImg;
+var colisor;
+var score = 0;
+
+
+
+function preload(){
+  backgroundImg = loadImage("backgroundImg.png");
+  borboleta1Img = loadImage("borboletaRosa.png");
+  borboleta2Img = loadImage("borboletaLaranja.png");
+  borboleta3Img = loadImage("borboletaAzulImg.png");
+  borboleta4Img = loadImage("borboletaRoxaImg.png");
+
+
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+
+  jogador = new Player(windowWidth/2,windowHeight/2 + 150,170,370);
+  
+  colisor = createSprite(windowWidth/2,windowHeight/2 + 150,170,370);
+
+
+
+  grupoBorboletas = new Group();
+
+
+
+}
+
+function draw() {
+  background(backgroundImg);  
+  Engine.update(engine);
+
+
+  fill("white");
+  textSize(40);
+  text("PEGUE AS BORBOLETAS ANTES QUE ELAS SOMEM!", 160,50);
+
+  fill("white");
+  textSize(45);
+  text("SCORE :"+score,40,250);
+
+
+  if(keyCode === UP_ARROW){
+    colisor.y = colisor.y -1;    
+      
+      }
+  if(keyCode === DOWN_ARROW){
+    colisor.y = colisor.y +1;    
+
+      }
+  if(keyCode === LEFT_ARROW){
+    colisor.x = colisor.x -1;    
+
+      }
+  if(keyCode === RIGHT_ARROW){
+    colisor.x = colisor.x +1;    
+
+        
+      }
+
+  
+  colisor.visible = false;
+
+  colisor.display();
+  jogador.display();
+  gerarBorboletas();
+ 
+
+  drawSprites();
+}
+
+function gerarBorboletas(){
+  if(frameCount % 90 == 0 ){
+
+    
+    var borboleta1 = createSprite(30, 130, 20,20);
+    borboleta1.velocityX = +5;
+    borboleta1.lifetime = 100;
+
+    var rand = Math.round(random(1,1) );
+    switch(rand){
+      
+      case 1 : 
+        borboleta1.addImage(borboleta1Img);
+      break
+    }
+    
+    if(grupoBorboletas.isTouching(colisor)){
+      score = score+1;
+    }
+
+    grupoBorboletas.add(borboleta1);
+  }
+
+  if(frameCount % 70 == 0){
+    var borboleta2 = createSprite(1200,140,20,20);
+    borboleta2.velocityX = -5;
+    borboleta2.lifetime = 100;
+
+    var rand = Math.round(random(1,1));
+    switch(rand){
+
+      case 1 :
+        borboleta2.addImage(borboleta3Img);
+      break
+    }
+    
+    if(grupoBorboletas.isTouching(colisor)){
+      score = score+1;
+    }
+    grupoBorboletas.add(borboleta2);
+  
+  }
+
+  if(score === 5){
+    fill("white");
+    textSize(35);
+    text("PARABÉNS! Você passou de nível", 150, 150);
+  }
+  
+  if(score >= 5){
+    if(frameCount % 60 == 0){
+      var borboleta3 = createSprite(30,300,20,20);
+      borboleta3.velocityX = +5;
+      borboleta3.lifetime = 60;
+
+      var rand = Math.round(random(1,1));
+      switch(rand){
+        case 1 :
+          borboleta3.addImage(borboleta4Img);
+        break
+      }
+
+      if(grupoBorboletas.isTouching(colisor)){
+        score = score+1;
+      }
+
+      grupoBorboletas.add(borboleta3);
+    }
+  }
+
+  if(score == 10){
+    fill("white");
+    textSize(35);
+    text("PARABÉNS! Você passou de nível", 150, 150);
+  }
+
+  if(score >= 10){
+    if(frameCount % 50 == 0){
+      var borboleta4 = createSprite(1200,350,20,20);
+      borboleta4.velocityX = -5;
+      borboleta4.lifetime = 60;
+
+      var rand = Math.round(random(1,1));
+      switch(rand){
+        case 1 :
+          borboleta4.addImage(borboleta2Img);
+        break
+      }
+
+      if(grupoBorboletas.isTouching(colisor)){
+        score = score+1;
+      }
+
+      grupoBorboletas.add(borboleta4);
+    }
+  }
+
+  
+}
+
